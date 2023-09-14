ca1bf740a37473cbe203ee40cb24ab45dbc01d29
skamuju <Saketh.S.Kamuju@gmail.com>
2023-09-14 18:45:04 -0400

    Switch to Flask

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,23 @@
 
 
-import sys
-from PySide2.QtWidgets import QApplication, QPushButton
+from flask import Flask
 
-app = QApplication(sys.argv)
+app = Flask(__name__)
 
-window = QPushButton("Push Me")
-window.show()
+flip = True
 
-app.exec_()
+@app.route('/')
+def welcome():
+    return 'Welcome to MindFlow!'
+
+@app.route('/switch')
+def switch():
+    if flip:
+        return 'True'
+    
+    else:
+        return 'False'
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
