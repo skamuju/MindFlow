220f38833fa9031e6a729f029ccec7bab25d4a10
skamuju <Saketh.S.Kamuju@gmail.com>
2023-09-21 00:59:10 -0400

    Finally install pymongo and create requirements.txt

--- backend/main.py
+++ backend/main.py
@@ -5,17 +5,22 @@ from pymongo import MongoClient, operations
 
 app = Flask(__name__)
 
-flip = True
-
 @app.route('/')
 def welcome():
     return 'Welcome to MindFlow!'
 
-@app.route('/register')
+@app.route('/register', methods = ['POST'])
 def register():
-    username = str(input())
-    password = str(input())
+    data = request.get.json()
+    my_client = MongoClient('mongodb+srv://skamuju:<QAtgV28nj9587Ewl>@cluster0.vdo8j3z.mongodb.net/?retryWrites=true&w=majority')
+    db = my_client['MindFlow']
+    collection = db['users']
 
+    post = {'username': data["username"], 'email': data["email"],
+            'password': data["password"], 'devices': [], "traffic": 1}
+
+    collection.insert_one(post)
+    return jsonify({"success": True})
 
 
 @app.route('/graph')
@@ -33,7 +38,6 @@ def meds():
 @app.route('/Resources')
 def recs():
     return "pending"
-
 
 if __name__ == "__main__":
     app.run(debug = True)
